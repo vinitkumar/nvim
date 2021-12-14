f92e9d296b41641883fa1fcf70e7f56b1c60fa3d
Vinit Kumar <mail@vinitkumar.me>
2021-12-14 14:08:21 +0530

    fix: some more deprecations

--- lua/config.lua
+++ lua/config.lua
@@ -49,7 +49,7 @@ opt.scrolloff = 3
 
 
 vim.cmd('filetype indent plugin on')
-vim.cmd('colorscheme onedarkpro')
+vim.cmd('colorscheme sitruuna')
 vim.cmd('syntax on')
 vim.g.mapleader = ","
 if vim.fn.filereadable('/usr/local/bin/python3') == 1 then
