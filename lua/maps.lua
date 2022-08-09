7fda54743888b9bf5bee7fcfe64cf9ad4c9f86b6
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2022-08-10 00:44:21 +0530

    feat: happy with this config for now

--- lua/maps.lua
+++ lua/maps.lua
@@ -2,15 +2,22 @@ vim.g.mapleader = ","
 local keymap = vim.keymap
 
 -- split settings
-keymap.set('n', '<Leader>h', ':<C-u>split<CR>', {noremap = true, silent=true})
-keymap.set('n', '<Leader>v', ':<C-u>vsplit<CR>', {noremap = true, silent=true})
+keymap.set('n', '<Leader>h', ':<C-u>split<CR>', { noremap = true, silent = true })
+keymap.set('n', '<Leader>v', ':<C-u>vsplit<CR>', { noremap = true, silent = true })
 
 
 -- tab settings
-keymap.set('n', '<Leader>t', ':<C-u>tabnew<CR>', {noremap = true, silent=true})
-keymap.set('n', '<C-t>', ':tabNext<CR>', {noremap = true, silent=true})
+keymap.set('n', '<Leader>t', ':<C-u>tabnew<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-t>', ':tabNext<CR>', { noremap = true, silent = true })
 
 
 -- Up and Down Mapping
-keymap.set('n', '<CR>', 'G', {noremap = true, silent=true})
-keymap.set('n', '<BS>', 'gg', {noremap = true, silent=true})
+keymap.set('n', '<CR>', 'G', { noremap = true, silent = true })
+keymap.set('n', '<BS>', 'gg', { noremap = true, silent = true })
+
+
+keymap.set('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-b>', ':Telescope buffers<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-l>', ':Telescope git_files<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-c>', ':Telescope git_commits<CR>', { noremap = true, silent = true })
+keymap.set('n', '<C-e>', ':Telescope diagnostics bufnr=0<CR>', { noremap = true, silent = true })
