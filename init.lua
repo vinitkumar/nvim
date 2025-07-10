43c58369510a2e9e2eb0b5d9d55a07819bbbe308
Vinit Kumar <vinit.kumar@promobitech.com>
2025-07-10 20:07:02 +0530

    update (#5)

--- init.lua
+++ init.lua
@@ -18,13 +18,8 @@ packer.startup(function (use)
   use {'neoclide/coc.nvim', branch = 'master', run = 'npm ci'}
   use 'tpope/vim-commentary'
   use 'github/copilot.vim'
-  use {
-    "tinted-theming/tinted-vim",
-    config = function()
-      vim.cmd.colorscheme 'base16-bright'
-    end,
-  }
   use 'duane9/nvim-rg'
+  use 'vinitkumar/oscura-vim'
   use 'craftzdog/solarized-osaka.nvim'
 
 end)
@@ -156,40 +151,6 @@ vim.opt.wildoptions = 'pum'
 vim.opt.list = true
 
 
-require("solarized-osaka").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  transparent = true, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
-  },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors) end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-})
-
 
 function StripTrailingWhitespace()
   if not vim.bo.binary and vim.bo.filetype ~= 'diff' then
@@ -218,7 +179,7 @@ function SwitchBackgroundAndColorScheme()
     vim.cmd("colorscheme solarized-osaka")
   else
     vim.opt.background = 'light'
-    vim.cmd("colorscheme solarized-osaka-day")
+    vim.cmd("colorscheme ambvlight")
   end
 end
 
